62d4811525ee7e565fc1f4904e05e5f672bcb34e
Brian Mwiruki <mwiruki@outlook.com>
2019-10-31 22:15:29 +0300

    portfolio section init

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+$('#design').click(function(){
+$('#design p').toggle();
+$('#design img').toggle();
+});
+$('#devt').click(function(){
+$('#devt p').toggle();
+$('#devt img').toggle();
+});
+$('#ptm').click(function(){
+$('#ptm p').toggle();
+$('#ptm img').toggle();
+});
