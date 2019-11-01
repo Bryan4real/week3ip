3e60fb838ecd5cffb584f7c2ad57498f65c89093
Brian Mwiruki <mwiruki@outlook.com>
2019-11-01 16:16:04 +0300

    portfolio hover add

--- js/main.js
+++ js/main.js
@@ -11,3 +11,27 @@ $('#ptm').click(function(){
 $('#ptm p').toggle();
 $('#ptm img').toggle();
 });
+$('.col-md-3 #w1').hover(function(){
+  $('#p1').toggle();
+});
+$('.col-md-3 #w2').hover(function(){
+  $('#p2').toggle();
+});
+$('.col-md-3 #w3').hover(function(){
+  $('#p3').toggle();
+});
+$('.col-md-3 #w4').hover(function(){
+  $('#p4').toggle();
+});
+$('.col-md-3 #w5').hover(function(){
+  $('#p5').toggle();
+});
+$('.col-md-3 #w6').hover(function(){
+  $('#p6').toggle();
+});
+$('.col-md-3 #w7').hover(function(){
+  $('#p7').toggle();
+});
+$('.col-md-3 #w8').hover(function(){
+  $('#p8').toggle();
+});
